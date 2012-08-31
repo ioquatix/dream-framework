43fe5fb39999efe5fa0f506ed0876ed4e28ab9d7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-08-31 15:54:49 +1200

    Added support for tagged-model-format package.

---
+++ packages/tagged-model-format/package.rb
@@ -0,0 +1,30 @@
+
+Package.define("tagged-model-format-0.1.0") do |package|
+	package.fetch_from :url => "https://github.com/ioquatix/tagged-model-format/tarball/0.1.0", :filename => "tagged-model-format-0.1.0.tar.gz"
+	
+	package.variant(:all) do |platform, config|
+		puts YAML::dump(config.build_environment)
+		
+		RExec.env(config.build_environment) do
+			Dir.chdir(package.source_path) do
+				sh("rm", "-rf", "build")
+				sh("mkdir", "build")
+				
+				Dir.chdir("build") do
+					cmake_options = []
+					cmake_options << "-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}"
+					cmake_options << "-DCMAKE_PREFIX_PATH=#{platform.prefix}"
+					cmake_options << ".."
+					
+					sh("cmake", "-G", "Unix Makefiles", *cmake_options)
+					
+					sh("make")
+					sh("make", "install")
+				end
+			end
+		end
+	end
+end
+
+
+
