291cd1fabdb6b1647a8392b10147c47a308e9c86
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-10-31 00:34:35 +1300

    Updated "Tagged Model Format" -> "Tagged Format".

--- packages/tagged-format/package.rb
+++ packages/tagged-format/package.rb
@@ -1,10 +1,8 @@
 
-Package.define("tagged-model-format-0.1.1") do |package|
-	package.fetch_from :url => "https://github.com/ioquatix/tagged-model-format/tarball/0.1.1", :filename => "tagged-model-format-0.1.1.tar.gz"
+Package.define("tagged-format-0.1.1") do |package|
+	package.fetch_from :url => "https://github.com/ioquatix/tagged-format/tarball/0.1.1", :filename => "tagged-format-0.1.1.tar.gz"
 	
 	package.build(:all) do |platform, config|
-		puts YAML::dump(config.build_environment)
-		
 		RExec.env(config.build_environment) do
 			Dir.chdir(package.source_path) do
 				sh("rm", "-rf", "build")
