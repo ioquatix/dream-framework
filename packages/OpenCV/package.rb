47915ca1f136d9634f8b7a40f9a8c11a14e63395
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-06 18:05:48 +1300

    Updated OpenCV to 2.4.3

--- packages/OpenCV/package.rb
+++ packages/OpenCV/package.rb
@@ -1,18 +1,18 @@
 
-Package.define("OpenCV-2.4.2") do |package|
-	package.fetch_from :url => "http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.2/OpenCV-2.4.2.tar.bz2"
+Package.define("OpenCV-2.4.3") do |package|
+	package.fetch_from :url => "https://github.com/itseez/opencv/archive/2.4.3.zip"
 	
 	package.build(:all) do |platform, config|
 		RExec.env(config.build_environment) do
-			run("patch", "-Np0", "-iclang.patch")
-			run("patch", "-Np0", "-ilsh.patch")
+			#run("patch", "-Np0", "-iclang.patch")
+			#run("patch", "-Np0", "-ilsh.patch")
 			
 			Dir.chdir(package.source_path) do
 				sh("rm", "-rf", "build")
 				sh("mkdir", "build")
 				
 				Dir.chdir("build") do
-					sh("cmake", "-G", "Unix Makefiles", "-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}", "-DCMAKE_CXX_COMPILER_WORKS=TRUE", "-DCMAKE_C_COMPILER_WORKS=TRUE", "-DBUILD_opencv_legacy=OFF", "-DBUILD_opencv_nonfree=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_highgui=OFF", "-DBUILD_SHARED_LIBS=OFF", "..")
+					sh("cmake", "-G", "Unix Makefiles", "-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}", "-DCMAKE_CXX_COMPILER_WORKS=TRUE", "-DCMAKE_C_COMPILER_WORKS=TRUE", "-DBUILD_opencv_legacy=OFF", "-DBUILD_opencv_nonfree=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_ts=OFF", "-DBUILD_opencv_objdetect=off", "-DBUILD_opencv_highgui=OFF", "-DBUILD_SHARED_LIBS=OFF", "..")
 					
 					sh("make")
 					sh("make", "install")
